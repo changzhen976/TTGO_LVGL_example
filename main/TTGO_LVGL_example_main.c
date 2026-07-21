c8f6952a0071fd3f55c761ef25d1f73a5daf4c38
changzhen976 <changzhen976@gmail.com>
2026-07-21 09:00:46 +0800

    drop redundant includes in app_main

--- main/TTGO_LVGL_example_main.c
+++ main/TTGO_LVGL_example_main.c
@@ -4,23 +4,13 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
-#include "esp_timer.h"
-#include "esp_lcd_panel_io.h"
-#include "esp_lcd_panel_vendor.h"
-#include "esp_lcd_panel_ops.h"
-#include "driver/gpio.h"
-#include "driver/spi_master.h"
 #include "esp_err.h"
-#include "esp_log.h"
-#include "lvgl.h"
 #include "lvglSpiCfg.h"
 #include "lv_port_indev.h"
 
-#include "multi_button.h"
 #include "wifiCfg.h"
 #include "ui.h"
 
